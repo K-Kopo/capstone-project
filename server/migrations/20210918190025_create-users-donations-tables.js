248642ceb8c2e0e80113b18ee209697d89690d74
Kory Koponen <koponenk@gmail.com>
2021-09-18 14:11:46 -0500

    created tables in database

---
+++ server/migrations/20210918190025_create-users-donations-tables.js
@@ -0,0 +1,34 @@
+
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('users', (table) => {
+      table.increments('id').primary();
+      table.string('name').notNullable();
+      table.string('username').notNullable().defaultTo('Store Manager');
+      table.string('password').notNullable();
+      table.string('role').notNullable();
+      table.integer('phone').notNullable();
+      table.string('email').notNullable();
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+    .createTable('inventories', (table) => {
+      table.increments('id').primary();
+      table.string('type').notNullable();
+      table.string('description').notNullable();
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      table.integer('amount').notNullable().defaultTo(0);
+      table.integer('expires').notNullable();
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('donations').dropTable('users');
+};
