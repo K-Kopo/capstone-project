5b47fb64d166ec417121c2863314044026562b93
Kory Koponen <koponenk@gmail.com>
2021-09-21 16:35:04 -0500

    work on user page

--- client/src/pages/UserPage/UserPage.js
+++ client/src/pages/UserPage/UserPage.js
@@ -7,6 +7,7 @@ const UserPage = () => {
     const [errorMessage, setErrorMessage] = useState("");
     const [userData, setUserData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [userDonations, setUserDonations] = useState([]);
   
 //    handleAuthFail = () => {
 //         sessionStorage.removeItem('authToken');
@@ -22,13 +23,21 @@ const UserPage = () => {
         }).then (res => {
             setUserData(res.data);
             setIsLoading(false);
+            console.log(res.data);
         }).catch (() => (sessionStorage.removeItem('authToken'), setLoggedIn(false)));
      
     }, []);
+    axios.get('http://localhost:5000/donations/')
+    .then(response => {
+        setUserDonations(response.data)
+    })
+    .catch((error) => console.log(error));
 
+    const donations = userDonations.filter(donation => donation.user_id === userData.id);
+    console.log(donations);
     return (
         isLoading ? <h1>PATIENCE IS A VIRTUE...</h1> :
-
+        
         <div>
             <h2>Hello again, {userData.name}</h2>
         </div>
--- server/routes/donations.js
+++ server/routes/donations.js
@@ -3,7 +3,53 @@ const router = express.Router();
 const Donation = require("../models/donation");
 const User = require("../models/user");
 
+// get single donation by id
+router
+    .route("/:id")
+    .get((req, res) => {
+        console.log(req.params.id);
+        Donation.where({ id: req.params.id})
+            .fetch({ withRelated: ["user"] })
+            .then((donation) => {
+                res.status(200).json({ donation });
+            })
+            .catch(() => {
+                res.status(400).json({ message: "Error, can't get donation" });
+            });
+    })
+    .put((req, res) => {
+        Donation.where({ id: req.params.id })
+            .fetch()
+            .then((donation) => {
+                donation
+                    .save({
+                        
+                        description: req.body.description,
+                        user_id: req.body.user_id,
+                        amount: req.body.amount,
+                        expires: req.body.expires,
+                        type: req.body.type
+                    })
+                    .then((updatedDonation) => {
+                        res.status(200).json(updatedDonation);
+                    })
+                    .catch(() => {
+                        res.status(400).json({ message: "Error, can't save donation" });
+                    });
+            });
+    })
+    .delete((req, res) => {
+        Donation.where("id", req.params.id)
+            .destroy()
+            .then((deletedDonation) => {
+                console.log(deletedDonation);
 
+                res.status(200).json({
+                    message: `Donation deleted successfully`,
+                });
+            })
+            .catch(() => res.status(400).json({ message: "Error, can't delete donation" }));
+    });
 // get all donations
 router
     .route("/")
@@ -48,52 +94,52 @@ router
             });
     });
 
-// get single inventory by id
-router
-    .route("/:id")
-    .get((req, res) => {
-        Donation.where({ id: req.params.id })
-            .fetch({ withRelated: ["user"] })
-            .then((donation) => {
-                res.status(200).json({ donation });
-            })
-            .catch(() => {
-                res.status(400).json({ message: "Error, can't get donation" });
-            });
-    })
-    .put((req, res) => {
-        Donation.where({ id: req.params.id })
-            .fetch()
-            .then((donation) => {
-                donation
-                    .save({
+// // get single inventory by id
+// router
+//     .route("/:id")
+//     .get((req, res) => {
+//         Donation.where({ id: req.params.id })
+//             .fetch({ withRelated: ["user"] })
+//             .then((donation) => {
+//                 res.status(200).json({ donation });
+//             })
+//             .catch(() => {
+//                 res.status(400).json({ message: "Error, can't get donation" });
+//             });
+//     })
+//     .put((req, res) => {
+//         Donation.where({ id: req.params.id })
+//             .fetch()
+//             .then((donation) => {
+//                 donation
+//                     .save({
                         
-                        description: req.body.description,
-                        user_id: req.body.user_id,
-                        amount: req.body.amount,
-                        expires: req.body.expires,
-                        type: req.body.type
-                    })
-                    .then((updatedDonation) => {
-                        res.status(200).json(updatedDonation);
-                    })
-                    .catch(() => {
-                        res.status(400).json({ message: "Error, can't save donation" });
-                    });
-            });
-    })
-    .delete((req, res) => {
-        Donation.where("id", req.params.id)
-            .destroy()
-            .then((deletedDonation) => {
-                console.log(deletedDonation);
+//                         description: req.body.description,
+//                         user_id: req.body.user_id,
+//                         amount: req.body.amount,
+//                         expires: req.body.expires,
+//                         type: req.body.type
+//                     })
+//                     .then((updatedDonation) => {
+//                         res.status(200).json(updatedDonation);
+//                     })
+//                     .catch(() => {
+//                         res.status(400).json({ message: "Error, can't save donation" });
+//                     });
+//             });
+//     })
+//     .delete((req, res) => {
+//         Donation.where("id", req.params.id)
+//             .destroy()
+//             .then((deletedDonation) => {
+//                 console.log(deletedDonation);
 
-                res.status(200).json({
-                    message: `Donation deleted successfully`,
-                });
-            })
-            .catch(() => res.status(400).json({ message: "Error, can't delete donation" }));
-    });
+//                 res.status(200).json({
+//                     message: `Donation deleted successfully`,
+//                 });
+//             })
+//             .catch(() => res.status(400).json({ message: "Error, can't delete donation" }));
+//     });
 
 
 module.exports = router;
